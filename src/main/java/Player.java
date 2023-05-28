0a494098691bea356d40de69e602a101d00a6ef3
Krasnikov <krastist@yandex.ru>
2023-05-28 23:36:17 +0500

    Intidl commit

---
+++ src/main/java/Player.java
@@ -0,0 +1,23 @@
+public class Player {
+    private int id;
+    private String name;
+    private int strangth;
+
+    public Player(int id, String name, int strangth) {
+        this.id = id;
+        this.name = name;
+        this.strangth = strangth;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getStrangth() {
+        return strangth;
+    }
+
+
+}
